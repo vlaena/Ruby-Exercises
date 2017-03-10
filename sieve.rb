faebe7a0aeb2c5f90e7cf5e64fdfefcc191c530a
vlaena <m.vladenache@yahoo.co.uk>
2017-03-10 09:42:41 +0200

    Update sieve.rb
    Added comments

--- sieve.rb
+++ sieve.rb
@@ -1,28 +1,28 @@
 class PrimeChecker
-	def initialize
-		p "Enter max number: "
-		@n = gets.chomp.to_i
-		@i = 2
-		@k = @n / @i
+	def initialize									
+		p "Enter max number: "							
+		@n = gets.chomp.to_i							#gets user input, sets maximum element of array
+		@i = 2									#initializes i, used later
+		@k = @n / @i								#value used in for loop later
 	end
 	
 	def create
-		@primes = Array(2..@n)
-		@primes
+		@primes = Array(2..@n)							#creates new array containing @n-1 elements
+		@primes								
 	end
 
 	def check
-		@primes.each do |x|
-			for @i in 2..@k do
-				el = (@i * x)
+		@primes.each do |x|							#iterates through the array, deletes multiples of x
+			for @i in 2..@k do						#and leaves only the prime numbers
+				el = (@i * x)						
 				@primes.delete(el)
 			end
 		end
 	end
 
-	def display
+	def display									
 		puts "The prime numbers between 2 and #{@n} are: #{@primes}."
-		puts "Number of primes is #{@primes.size}."
+		puts "Number of primes is #{@primes.size}."				#displays array size for verifying the result
 	end
 end
 
